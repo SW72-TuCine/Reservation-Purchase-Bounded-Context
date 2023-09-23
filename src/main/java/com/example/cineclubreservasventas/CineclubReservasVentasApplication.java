e302454d9415415bb42b614ca9235565344aeb3b
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-09-22 23:08:02 -0500

    Added Application and ApplicationTests

---
+++ src/main/java/com/example/cineclubreservasventas/CineclubReservasVentasApplication.java
@@ -0,0 +1,20 @@
+package com.example.cineclubreservasventas;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class CineclubReservasVentasApplication {
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CineclubReservasVentasApplication.class, args);
+	}
+
+}
