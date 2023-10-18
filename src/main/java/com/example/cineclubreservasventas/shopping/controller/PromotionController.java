e20a4df95de66991a907a0c42a066f7b2a829c3f
Axel Fiestas <josue123452002@gmail.com>
2023-10-18 13:49:39 -0500

    feat: Connection with API Gateway and Eureka Done

--- src/main/java/com/example/cineclubreservasventas/shopping/controller/PromotionController.java
+++ src/main/java/com/example/cineclubreservasventas/shopping/controller/PromotionController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/TuCine/v1")
+@RequestMapping("/api/TuCine/v1/reservation_purchase")
 public class PromotionController {
     @Autowired
     private PromotionService promotionService;
